5dba8162999c99d4e38001823f803fb18c64f655
Paul <paulfantom@gmail.com>
2015-10-25 13:02:31 +0100

    fixed issue #1

--- tea.sh
+++ tea.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 wait() {
     suffix=$(echo "$1" | tail -c 2)
-    number=${1::-1}
+    if [[ ! ${suffix} =~ [smdh] ]]; then
+        suffix="s"
+        number=$1
+    else
+        number=${1::-1}
+    fi
     while [ $(echo "$number > 0" | bc) -eq 1 ]; do
         echo "$number$suffix to go"
         if [ $(echo "$number>1" | bc) -eq 1 ]; then
@@ -18,6 +23,8 @@ if [ "$#" -lt 1 ]; then
     echo "Usage: tea NUMBER[SUFFIX]... (like sleep)"
     exit 1
 fi
+
 for i; do wait $i; done
+eject
 notify-send -a tea -i task-attempt "Tea is ready" "Hurray!!!"
 paplay /usr/share/sounds/freedesktop/stereo/bell.oga
